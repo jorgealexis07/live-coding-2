ceb0bbc5371d3a227b4e9139309fc0c1ae590a44
jorgealexis07 <201711104@tese.edu.mx>
2023-01-13 11:38:46 -0600

    Subo prueba tecnica resuelta

---
+++ app.js
@@ -0,0 +1,13 @@
+let btn = document.getElementById("btn");
+let containerIMG= Document.getElementById("containerIMG");
+
+function fetchAnswer(){
+    let dato = fetch("https://yesno.wtf/api");
+
+}
+
+btn.addEventListener("click",function (params) {
+    da
+});
+
+
